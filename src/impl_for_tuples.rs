d13b4d38a624e6f0baa457bb2eda1c898ecca585
melvdl <hansw533@gmail.com>
2024-08-25 02:55:11 +0200

    pass field index alongside field type

--- src/impl_for_tuples.rs
+++ src/impl_for_tuples.rs
@@ -1,17 +1,19 @@
 use itertools::Itertools;
-use proc_macro2::TokenStream;
+use proc_macro2::{Literal, TokenStream};
 use quote::{format_ident, quote};
 use syn::{LitInt, Result};
 
 pub fn impl_for_tuples(args: proc_macro::TokenStream) -> Result<TokenStream> {
     let arity = syn::parse::<LitInt>(args)?.base10_parse()?;
-    let names = (0..arity).map(|n| format_ident!("T{n}"));
+    let (fields, names): (Vec<_>, Vec<_>) = (0..arity)
+        .map(|n| (Literal::usize_unsuffixed(n), format_ident!("T{n}")))
+        .unzip();
 
     let impls = (0..arity)
-        .map(|arity| names.clone().take(arity))
-        .map(|names| {
+        .map(|arity| (fields.iter().take(arity), names.iter().take(arity)))
+        .map(|(fields, names)| {
             quote! {
-                impl_for_tuple!((#(#names,)*));
+                impl_for_tuple!((#(#fields: #names,)*));
             }
         })
         .collect_vec();
